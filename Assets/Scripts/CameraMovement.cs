9215101024e60b1a30fd72fb72a15dfddc6b2da4
Gian Hancock <qwertyman@hotmail.com.au>
2014-08-30 20:21:57 +0100

    initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public Transform cameraTransform;
+	public Vector3 relativePosition;
+	public float xSensitivity;
+	public float ySensitivity;
+	public bool lockCursor;
+
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = lockCursor;
+		cameraTransform.position = transform.position + relativePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		relativePosition = Quaternion.Euler (0, Input.GetAxis ("Mouse X") * xSensitivity * Time.deltaTime, 0) * relativePosition;
+
+		cameraTransform.RotateAround (transform.position, Vector3.up, Input.GetAxis("Mouse X")*xSensitivity*Time.deltaTime);
+		cameraTransform.position = transform.position + relativePosition;
+	}
+}
---
+++ Assets/Scripts/force.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class force : MonoBehaviour {
+	public float forceCoeff;
+	public float jumpCoeff;
+	public float torqueCoeff;
+
+	private Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		cameraTransform = gameObject.GetComponent <CameraMovement>().cameraTransform;
+	}
+
+	void FixedUpdate(){
+		// translational forces
+		Vector3 fForce = Input.GetAxisRaw ("Vertical") * Vector3.Scale (cameraTransform.forward, new Vector3 (1, 0, 1));
+		Vector3 hForce = Input.GetAxisRaw ("Horizontal") * Vector3.Scale (cameraTransform.right, new Vector3 (1, 0, 1));
+		Vector3 tForce = (fForce + hForce).normalized * forceCoeff;
+
+		// torques
+		Vector3 fTorque = Input.GetAxisRaw ("Vertical") * (Vector3.Scale (cameraTransform.right, new Vector3 (1, 0, 1))).normalized * torqueCoeff;
+		Vector3 hTorque = Input.GetAxisRaw("Horizontal")* (Vector3.Scale (cameraTransform.forward, new Vector3(1, 0, 1))).normalized * -torqueCoeff;
+		Vector3 tTorque = (fTorque + hTorque).normalized * torqueCoeff;
+
+		// jumping
+		if (Input.GetKeyDown (KeyCode.Mouse0) || Input.GetKeyDown (KeyCode.Space))
+		{
+			rigidbody.AddForce (Physics.gravity.normalized * -jumpCoeff, ForceMode.Impulse);
+		}
+
+		// force/torque application
+		rigidbody.AddForce (tForce, ForceMode.Force);
+		rigidbody.AddTorque (tTorque, ForceMode.Force);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
